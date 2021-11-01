ae130610846495c6ade9a274258d46b48fae9eab
Anup Jacob <L00163455@student.lyit.ie>
2021-11-01 11:04:55 +0000

    First Commit

---
+++ Q1.py
@@ -0,0 +1,11 @@
+# ---------------------------------
+
+# File          : Q1.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 01/11/2021
+# Modified Date : 01/11/2021
+# Description   : Sample for Annotation
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
+
